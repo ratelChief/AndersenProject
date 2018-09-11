718a16336651f2f222681ea1abaf6b111e6c2940
ASD <dima1002010@live.com>
2018-09-11 17:15:46 +0300

    init

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,8 @@
 module.exports = {
   entry: "./home", //что собирать
   output: {
-  //  path: __dirname + "/dist", // куда выводить
-    filename: "build.js" // имя файла
+    path: __dirname + "/public", // куда выводить
+    filename: "build.js", // имя файла
+    library: "home"
   }
 }
--- welcome.js
+++ welcome.js
@@ -1,5 +1,5 @@
 'use strict';
 
 module.exports = function(message) {
-  console(`welcome ${message}`);
+  console.log(`welcome ${message}`);
 };
