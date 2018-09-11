9d0eb090cdcaff423c5aed2f825517bd6e8fa4ec
ASD <dima1002010@live.com>
2018-09-11 17:56:52 +0300

    init

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,27 @@
 'use strict';
 
+const NODE_ENV = process.env.NODE_ENV || 'development';
+const webpack = require('webpack');
+
 module.exports = {
   entry: "./home", //что собирать
   output: {
     path: __dirname + "/public", // куда выводить
     filename: "build.js", // имя файла
     library: "home"
-  }
-}
+  },
+
+  watch: NODE_ENV == 'development', //включаем вотчер, только если окружение - девелопмент
+
+  watchOptions: {
+    aggregateTimeout: 100
+  },
+
+  devtool: NODE_ENV == 'development' ? "source-map" : null, // соурс мэп работает только, если окружение - девелопмнт, если продакшн - то соурс мэп отсутствует
+
+  plugins : [
+    new webpack.DefinePlugin({
+      NODE_ENV: JSON.stringify(NODE_ENV)
+    })
+  ]
+};
--- welcome.js
+++ welcome.js
@@ -1,5 +1,8 @@
 'use strict';
 
 module.exports = function(message) {
+  if(NODE_ENV == 'development'){
+    console.log(message);
+  }
   console.log(`welcome ${message}`);
 };
