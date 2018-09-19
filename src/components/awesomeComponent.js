fa33434e2eff9becc5e07608716686c2dba60231
ASD <dima1002010@live.com>
2018-09-19 12:08:27 +0300

    redux is setted

--- src/components/awesomeComponent.js
+++ src/components/awesomeComponent.js
@@ -4,27 +4,12 @@ import { connect } from 'react-redux';
 
 class AwesomeComponent extends Component {
 
-  constructor() {
-    super();
-    this.addLike = this.addLike.bind(this);
-    this.removeLike = this.removeLike.bind(this);
-  }
-
-  addLike() {
-    this.props.onAddLike(this.props.testStore.likesCount);
-  }
-
-  removeLike() {
-    this.props.onRemoveLike(this.props.testStore.likesCount);
-  }
-
-
   render() {
     return (
       <div>
       <div className='likes'>{this.props.testStore.likesCount}</div>
-      <button className='addLike' onClick={this.addLike}>Add like</button>
-      <button className='removeLike' onClick={this.removeLike}>Remove like</button>
+      <button className='addLike' onClick={this.props.onAddLike}>Add like</button>
+      <button className='removeLike' onClick={this.props.onRemoveLike}>Remove like</button>
       </div>
     );
   }
