55e89e9d791fc8fade8e1177cbf2d0fb9beb1a2d
asdHome <dima1002010@live.com>
2018-09-20 23:09:09 +0300

    making status-bar

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-export const Form = ({ status }) =>
+export const Form = ({ data }) =>
 <form className='page-form'>
   <input type='text' className='search-input' />
   <button className='btn btn--search'>Go</button>
   <button className='btn btn--location'>My location</button>
-  <span className='status-bar'>{status}</span>
+  <span className='status-bar'>{data}</span>
   <fieldset className='results' />
 </form>;
