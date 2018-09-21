fb3c0c208e7cd35819cc4350bd31ad7d4b5aec6c
ASD <dima1002010@live.com>
2018-09-21 16:18:40 +0300

    using api

--- src/actions/locationActions.js
+++ src/actions/locationActions.js
@@ -2,9 +2,8 @@ export const FETCH_LOCATIONS = 'FETCH_LOCATIONS';
 export const FETCH_LOCATIONS_SUCCESS = 'FETCH_LOCATIONS_SUCCESS';
 export const FETCH_LOCATIONS_ERROR = 'FETCH_LOCATIONS_ERROR';
 
-export const fetchLocation = () => {
-  return dispatch => {
-    fetch('https://api.nestoria.co.uk/api?country=uk&pretty=1&action=search_listings&encoding=json&listing_type=buy&page=1&place_name=leeds', { mode: 'cors' })
+export const fetchLocation = () => dispatch => {
+    fetch('https://api.nestoria.co.uk/api?country=uk&pretty=1&action=search_listings&encoding=json&listing_type=buy&page=1&place_name=leeds')
     .then(res => res.json())
     .then(locations => {
       dispatch({
@@ -13,4 +12,3 @@ export const fetchLocation = () => {
       });
     });
   };
-};
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import './style.less';
 import { connect } from 'react-redux';
 import { Header } from '../components/Header';
 import { Main } from '../components/Main';
@@ -9,34 +8,35 @@ import { fetchLocation } from '../actions/locationActions';
 class App extends Component {
 
   render() {
-    const { getStatus, onGoButton, getData, locationsArray } = this.props;
+    const { getStatus, onGoButton, locationsArray } = this.props;
     const locationList = locationsArray.map(location =>
-    <a href='#' key={location.place_name}>{location.title}</a>);
-    return (
-      <div>
-        <Header />
-        <Main data={getStatus} onGoButton={onGoButton} locationList = {locationList}/>
-      </div>
-    );
+      <a href='#' key={location.place_name}>{location.title}</a>);
+      return (
+        <div>
+          <Header />
+          <Main
+            data={ getStatus }
+            onGoButton={ onGoButton }
+            locationList={ locationList }
+          />
+        </div>
+      );
+    }
   }
-}
 
-const mapStateToProps = state => ({
-  getStatus: state.statusBarReducer.status,
-  locationsArray: state.fetchLocations.locations
-});
+  const mapStateToProps = state => ({
+    getStatus: state.statusBarReducer.status,
+    locationsArray: state.fetchLocations.locations
+  });
 
-const mapDispatchToProps = dispatch => ({
-  onGoButton: () => {
-    dispatch(toggleStatus.toggleStatusOnSuccess());
-    dispatch(fetchLocation());
-  },
-  getData: () => {
-    dispatch(fetchLocation());
-  }
-});
+  const mapDispatchToProps = dispatch => ({
+    onGoButton: () => {
+      dispatch(toggleStatus.toggleStatusOnSuccess());
+      dispatch(fetchLocation());
+    }
+  });
 
-export default connect(
-  mapStateToProps, //mapStateToProps - мапит состояние стора в пропс компонента
-  mapDispatchToProps //mapDispatchToProps
-)(App);
+  export default connect(
+    mapStateToProps, //mapStateToProps - мапит состояние стора в пропс компонента
+    mapDispatchToProps //mapDispatchToProps
+  )(App);
