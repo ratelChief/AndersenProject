4c7e3c3ea5f64750b91e332ec9deb2de93ca9267
ASD <dima1002010@live.com>
2018-09-24 17:04:10 +0300

    search by input value is working

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -10,14 +10,13 @@ export class Form extends React.Component {
   }
 
   onGoButtonClick = () => {
-    debugger;
     this.props.onGoButton(this.state.placeName);
   }
 
   render() {
     return (
       <form className='page-form'>
-        <input type='text' className='search-input' onChange={this.onInputChanges} />
+        <input type='text' className='search-input' onChange={this.onInputChange} />
         <button type='button' className='btn btn--search' onClick={this.onGoButtonClick}>Go</button>
         <button className='btn btn--location'>My location</button>
         <span className='status-bar'>{this.props.data}</span>
