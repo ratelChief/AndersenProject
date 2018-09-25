51aba0f565e97a91890e3bde5c80c1d37ffcabbb
ASD <dima1002010@live.com>
2018-09-25 13:10:42 +0300

    implementing middleware

--- src/reducers/locationReducer.js
+++ src/reducers/locationReducer.js
@@ -1,26 +1,42 @@
 import { FETCH_LOCATIONS, FETCH_LOCATIONS_SUCCESS, FETCH_LOCATIONS_ERROR } from '../actions/locationActions';
 
 const initialState = {
+  fetching: false,
+  fetched: false,
+  error: null,
   locations: [],
-  title: 'Recent searches'
+  title: 'Recent searches:'
 };
 
 export const locationsReducer = (state = initialState, action) => {
   switch (action.type) {
+
     case FETCH_LOCATIONS:
     state = {
       ...state,
-      locations: action.payload.response.locations,
-      title: 'Please select a location below'
+      fetching: true
     };
     break;
+
+    case FETCH_LOCATIONS_SUCCESS:
+    state = {
+      ...state,
+      fetching: false,
+      fetched: true,
+      locations: action.payload.response.locations,
+      title: 'Please select a location below:'
+    };
+    break;
+
     case FETCH_LOCATIONS_ERROR:
     state = {
       ...state,
-      locations: [],
+      fetching: false,
+      error: action.payload,
       title: 'There was a problem with your search'
     };
     break;
+
     default: return state;
   }
   return state;
