5a9fad2049079a9853f64bc7fefedf595f01fcb6
asdHome <dima1002010@live.com>
2018-09-30 20:19:42 +0300

    routering-issue

--- src/components/SearchResults.jsx
+++ src/components/SearchResults.jsx
@@ -1,17 +1,26 @@
 import React, { Component } from 'react';
+import uuidv4 from 'uuid/v4';
 
 export default class SearchResults extends Component {
 
   renderSearchList = () => {
     if (this.props.searchList) {
-      return this.props.searchList.map(result => result.title);
+      return this.props.searchList.map(result =>
+        <div key={uuidv4()}>
+          <img src={result.img_url} />
+          <span>{result.price_formatted}</span>
+          <span>{result.title}</span>
+        </div>
+      );
     }
   }
 
   render() {
     console.log(this.props);
     return (
-      <div> {this.renderSearchList()} </div>
+      <div>
+        {this.renderSearchList()}
+      </div>
     );
   }
 }
