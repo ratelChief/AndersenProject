4eb9f44bc56e0c54cd22ccfa07c490b57cca4526
ASD <dima1002010@live.com>
2018-10-02 09:33:35 +0300

    added composition of components

--- src/actions/realtyActions.js
+++ src/actions/realtyActions.js
@@ -1,6 +1,6 @@
 import * as constants from '../constants/location.constants';
 
 export const fetchRealtyAction = placeName => ({
-  type: constants.FETCH_LOCATIONS,
+  type: constants.FETCH_REALTY_LIST,
   request: fetch(`https://api.nestoria.co.uk/api?country=uk&pretty=1&action=search_listings&encoding=json&listing_type=buy&page=1&place_name=${placeName}`)
 });
--- src/constants/location.constants.js
+++ src/constants/location.constants.js
@@ -5,3 +5,5 @@ export const FETCH_MY_LOCATION = 'FETCH_MY_LOCATION';
 export const FETCH_MY_LOCATION_SUCCESS = 'FETCH_MY_LOCATION_SUCCESS';
 
 export const RECENT_SEARCHES = 'recentSearches';
+
+export const FETCH_REALTY_LIST = 'FETCH_REALTY_LIST';
