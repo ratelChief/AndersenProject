45f88146554d263af1a920274ed80f4add929857
ASD <dima1002010@live.com>
2018-10-03 14:02:45 +0300

    added basic functionality for realty item

--- src/containers/PropertyDetails.js
+++ src/containers/PropertyDetails.js
@@ -3,7 +3,12 @@ import { connect } from 'react-redux';
 import PropertyDetails from '../components/propertyDetails/PropertyDetails.jsx';
 
 const mapStateToProps = state => ({
-  title: state.title
+  title: state.title,
+  price: state.price,
+  image: state.image,
+  desc: state.desc,
+  bathrooms: state.bathrooms,
+  bedrooms: state.bedrooms
 });
 
 export default connect(
--- src/reducers/PropertyReducer.js
+++ src/reducers/PropertyReducer.js
@@ -1,5 +1,10 @@
 
 const initialState = {
-  title: 'property details'
+  title: 'property details',
+  price: 'property price',
+  image: 'property image',
+  desc: 'property desc',
+  bedrooms: 0,
+  bathrooms: 0
 };
 export const propertyReducer = (state = initialState, action) => state;
