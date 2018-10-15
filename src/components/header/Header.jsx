ec502f54d2f5e098ede0b0371c58af6b5733cb76
asdHome <dima1002010@live.com>
2018-10-15 23:17:48 +0300

    added basic settings

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,7 +5,10 @@ import { Link } from 'react-router-dom';
 export const Header = () =>
   <header className={styles.pageHeader}>
     <h1 className={styles.pageLogo}>PropertyCross</h1>
-    <Link to='/favorites' className={styles.btnFaves}>
+    <Link to='/favorites' className={`${styles.btn} ${styles.faves}`}>
       Faves
+    </Link>
+    <Link to='/todolist' className={`${styles.btn} ${styles.todo}`}>
+      TodoList
     </Link>
   </header>;
--- src/components/todoList/TodoList.jsx
+++ src/components/todoList/TodoList.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
 
-export const todo = props => (
-  <ul>
-    <TodoItem />
-  </ul>
-);
+export class TodoList extends React.Component {
+
+  componentDidMount() {
+    console.log('mounted');
+  }
+
+  render() {
+
+    const btnAdd = '+';
+    const btnRemove = '-';
+
+    return (
+      <div>
+        <input type='text' />
+        <ul>
+          <li>value{btnAdd}{btnRemove}</li>
+        </ul>
+      </div>
+    );
+  }
+}
