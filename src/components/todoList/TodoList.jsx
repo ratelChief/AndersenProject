c466e5b619373738afa880fcbf17158b5d0321ba
ASD <dima1002010@live.com>
2018-10-18 15:19:22 +0300

    creating list item is correct

--- src/components/todoList/TodoList.jsx
+++ src/components/todoList/TodoList.jsx
@@ -1,24 +1,31 @@
 import React from 'react';
+import uuidv4 from 'uuid/v4';
 
 export class TodoList extends React.Component {
 
+  state = { placeName: '' };
+
   onInputChange = evt => {
-    const value = evt.target.value;
-  };
+    this.setState({
+      placeName: evt.target.value
+    });
+  }
 
   onAddBtn = () => {
-
+    this.props.addTodo(this.state.placeName);
   }
 
   render() {
     const btnAdd = <button onClick={this.onAddBtn}>+</button>;
     const btnRemove = <button>-</button>;
 
+    const renderItem = this.props.todoList.map(item => <li key={uuidv4()}>{item}{btnRemove}</li>);
+
     return (
       <div>
         <input onChange={this.onInputChange} type='text' /> {btnAdd}
         <ul>
-          <li>value{btnRemove}</li>
+          {renderItem}
         </ul>
       </div>
     );
